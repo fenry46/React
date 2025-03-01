eb98e7b6f89402d30b7ffabe407eeaacfe137d05
Fendy <76032813+fenry46@users.noreply.github.com>
2025-03-01 11:34:28 +0700

    React Learn Rendering List

--- my-react-app/src/Pages/products.jsx
+++ my-react-app/src/Pages/products.jsx
@@ -1,28 +1,44 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Nike Jordan",
+    price: "Rp 700.000",
+    image: "/images/shoes-2.jpg",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad`,
+  },
+  {
+    id: 2,
+    name: "Nike Air",
+    price: "Rp 500.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad`,
+  },
+  {
+    id: 3,
+    name: "Adidas Samba",
+    price: "Rp 2.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad`,
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Nike Air Dunk">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.{" "}
-        </CardProduct.Body>
-        <CardProduct.Footer price="RP 500.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-2.jpg" />
-        <CardProduct.Body title="Adidas Stan Smith">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.{" "}
-        </CardProduct.Body>
-        <CardProduct.Footer price="RP 700.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
--- my-react-app/src/components/Fragments/CardProduct.jsx
+++ my-react-app/src/components/Fragments/CardProduct.jsx
@@ -4,19 +4,19 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-600 border border-gray-400 rounded-lg shadow mx-2">
+    <div className="w-full max-w-sm bg-gray-600 border border-gray-400 rounded-lg shadow mx-2 flex flex-col justify-between">
       {children}
     </div>
   );
 };
 
 const Body = (props) => {
-  const { children, title } = props;
+  const { children, name } = props;
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <a href="#">
         <h5 className="text-xl font-semibold tracking-tight text-white">
-          {title}
+          {name}
         </h5>
         <p className="text-s text-white">{children}</p>
       </a>
