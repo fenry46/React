c1e0edfece104bd42209412a1d683c17cd261de6
Fendy <76032813+fenry46@users.noreply.github.com>
2025-03-10 21:23:48 +0700

    Learn Redux and Redux Toolkit

---
+++ my-react-app/slice.js
@@ -0,0 +1,38 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit"
+
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: [],
+    reducers: {
+        addToCart(state, action) {
+            state.push(action.payload)
+        }
+    }
+})
+const loginSlice = createSlice({
+    name: 'login',
+    initialState: { status: false },
+    reducers: {
+        login(state, action) {
+            state.status = true
+        }
+    }
+})
+
+const store = configureStore({
+    reducer: {
+        cart: cartSlice.reducer,
+        login: loginSlice.reducer
+    }
+})
+
+console.log("oncreate Store:", store.getState());
+
+store.subscribe(() => {
+    console.log("STORE CHANGE:", store.getState())
+})
+
+store.dispatch(cartSlice.actions.addToCart({ id: 1, qty: 20 }))
+store.dispatch(cartSlice.actions.addToCart({ id: 2, qty: 10 }))
+store.dispatch(loginSlice.actions.login())
